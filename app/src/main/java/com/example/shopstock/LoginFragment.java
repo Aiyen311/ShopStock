d8a53b002cbc27f1201e767b33ed4e5130c4e715
Adithya Iyengar <adithyasiyengar@gmail.com>
2020-06-19 20:27:59 -0400

    login and signup framework

--- app/src/main/java/com/example/shopstock/LoginFragment.java
+++ app/src/main/java/com/example/shopstock/LoginFragment.java
@@ -1,29 +1,131 @@
 package com.example.shopstock;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
-
+public class LoginFragment extends Fragment implements View.OnClickListener{
+    private static final String TAG = "Login";
+    private static final int REQUEST_SIGNUP = 0;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
+    @BindView(R.id.btn_login) Button login_button;
+    @BindView(R.id.et_username) EditText login_username;
+    @BindView(R.id.et_password) EditText login_password;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        login_button = view.findViewById(R.id.btn_login);
+        login_username = view.findViewById(R.id.et_username);
+        login_password = view.findViewById(R.id.et_password);
+
+        login_button.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v){
+        login();
+    }
+
+    private void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        login_button.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String username = login_username.getText().toString();
+        String password = login_password.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.getActivity().finish();
+            }
+        }
+    }
+
+     public void onLoginSuccess() {
+        login_button.setEnabled(true);
+        getActivity().finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getActivity().getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        login_button.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String username = login_username.getText().toString();
+        String password = login_password.getText().toString();
+
+        if (username.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(username).matches()) {
+            login_username.setError("enter a valid email address");
+            valid = false;
+        } else {
+            login_username.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            login_password.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            login_password.setError(null);
+        }
+
+        return valid;
+    }
 }
+
--- app/src/main/java/com/example/shopstock/RegisterFragment.java
+++ app/src/main/java/com/example/shopstock/RegisterFragment.java
@@ -1,29 +1,145 @@
 package com.example.shopstock;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RegisterFragment extends Fragment {
-
+public class RegisterFragment extends Fragment implements View.OnClickListener {
+    private static final String TAG = "SignupFragment";
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
+    @BindView(R.id.btn_register) Button register_button;
+    @BindView(R.id.et_name) EditText register_name;
+    @BindView(R.id.et_email) EditText register_email;
+    @BindView(R.id.et_password) EditText register_password;
+    @BindView(R.id.et_repassword) EditText register_re_password;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view = (View) inflater.inflate(R.layout.fragment_register, container, false);
+        register_button = (Button) view.findViewById(R.id.btn_register);
+        register_name = (EditText) view.findViewById(R.id.et_name);
+        register_email = (EditText) view.findViewById(R.id.et_email);
+        register_password = (EditText) view.findViewById(R.id.et_password);
+        register_re_password = (EditText) view.findViewById(R.id.et_repassword);
+
+        register_button.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        signup();
+    }
+
+    private void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        register_button.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = register_name.getText().toString();
+        String email = register_email.getText().toString();
+        String password = register_password.getText().toString();
+        String re_password = register_re_password.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        register_button.setEnabled(true);
+        getActivity().setResult(RESULT_OK, null);
+        getActivity().finish();
+        // TODO: Move from register fragment to login fragment when successful signup happens
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getActivity().getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        register_button.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = register_name.getText().toString();
+        String email = register_email.getText().toString();
+        String password = register_password.getText().toString();
+        String re_password = register_re_password.getText().toString();
+
+        if (name.isEmpty() || name.length() < 2) {
+            register_name.setError("at least 2 characters");
+            valid = false;
+        } else {
+            register_name.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            register_email.setError("enter a valid email address");
+            valid = false;
+        } else {
+            register_email.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            register_password.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            register_password.setError(null);
+        }
+
+        if (re_password.isEmpty() || !re_password.equals(password)) {
+            register_re_password.setError("re-typed password does not match");
+            valid = false;
+        } else {
+            register_re_password.setError(null);
+        }
+
+        return valid;
     }
 
 }
